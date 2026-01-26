c99ecc05b8454cf3ea533bca0b35860f5dc181e8
msivasubramaniaan <msivasub@redhat.com>
2026-01-27 00:04:51 +0530

    Remove emoji from greeting message in app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
 import sys
 
 name = sys.argv[1] if len(sys.argv) > 1 else "Devfile"
-print(f"Hello {name}, from Python Devfile playground 🐍")
+print(f"Hello {name}, from Python Devfile playground!")
